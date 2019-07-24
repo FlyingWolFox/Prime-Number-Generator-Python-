9b954d947fbc7ae2ab2aa80b7cc0c87fbd86b67a
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2019-07-24 09:43:09 -0300

    Retiring the initial division
    I retired the initial division. It appears to increase the execution time

--- primes.py
+++ primes.py
@@ -2,19 +2,18 @@ n = 2
 m = 1
 rest = []
 print ("2 is prime")
-while(n < 1000):
-	if (n%2 != 0 and n%5 != 0):
-		while(m<n):
-			p = n%m
-			rest = rest + [p]
-			count = rest.count(0)
-			m += 1
-			if (count < 2):
-				continue
-			else:
-				break
+while(n < 3000):
+	while(m<n):
+		p = n%m
+		rest = rest + [p]
+		count = rest.count(0)
+		m += 1
 		if (count < 2):
-			print(n,"is prime")
+			continue
+		else:
+			break
+	if (count < 2):
+		print(n,"is prime")
 	n += 1
 	m = 1
 	rest = []
